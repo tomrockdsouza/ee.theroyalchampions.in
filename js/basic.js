2599a739a87e04d54b917bae719d25c415b83f75
MediaForce <mediaforce13@gmail.com>
2022-07-31 18:40:56 +0530

    first commit

---
+++ js/basic.js
@@ -0,0 +1,24 @@
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera 
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
+
+// Prevents page from being framed
+if (window != window.top) {
+    top.location.href = location.href;
+}
